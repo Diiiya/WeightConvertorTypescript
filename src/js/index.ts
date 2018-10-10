8b58f23669d8d0b5e6e3813f57f1498c91975b7d
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-10-10 10:31:38 +0300

    ..

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,18 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let input: HTMLInputElement = <HTMLInputElement> document.getElementById("input");
+let buttonToOunces: HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonToOunces");
+buttonToOunces.addEventListener("click", ConvertToOunces);
+let buttonToGrams: HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonToGrams");
+buttonToGrams.addEventListener("click", ConvertToGrams);
+let divElement: HTMLDivElement = <HTMLDivElement> document.getElementById("output");
+
+function ConvertToOunces(): void {
+    let inputVal: string = input.value;
+    let result: number = (parseInt(inputVal) * 0.035274);
+    divElement.innerHTML = "The amount in ounces is: " + result.toString();
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function ConvertToGrams(): void {
+    let inputVal: string = input.value;
+    let result: number = (parseInt(inputVal) * 28.3495);
+    divElement.innerHTML = "The amount in grams is: " + result.toString();
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
